faf2da97c1d0825235f76f57e8a0699a43c965ea
ClementPtt <clement.poutret@gmail.com>
2020-10-15 16:43:18 +0200

    Adding Tkinter

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print('Hello World')
+from tkinter import *
+
+# On crée une fenêtre, racine de notre interface
+fenetre = Tk()
+
+# On crée un label (ligne de texte) souhaitant la bienvenue
+# Note : le premier paramètre passé au constructeur de Label est notre
+# interface racine
+champ_label = Label(fenetre, text="Salut les Zér0s !")
+
+# On affiche le label dans la fenêtre
+champ_label.pack()
+
+bouton_quitter = Button(fenetre, text="Quitter", command=fenetre.quit)
+bouton_quitter.pack()
+
+cadre = Frame(fenetre, width=768, height=576, borderwidth=1)
+cadre.pack(fill=BOTH)
+
+message = Label(cadre, text="Notre fenêtre")
+message.pack(side="top", fill=X)
+
+
+# On démarre la boucle Tkinter qui s'interompt quand on ferme la fenêtre
+fenetre.mainloop()
