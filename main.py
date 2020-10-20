3c5642e581c06339a8e41963eaf8f8e49d58a627
lolorahaingo <lolorahaingo@hotmail.fr>
2020-10-20 17:06:47 +0200

    modif de clarté

--- main.py
+++ main.py
@@ -1,30 +1,36 @@
 # @Date:   2020-10-15T19:31:27+02:00
-# @Last modified time: 2020-10-20T00:12:44+02:00
+# @Last modified time: 2020-10-20T17:05:53+02:00
 
 from tkinter import *
 
-# On crée une fenêtre
+# Crée une fenêtre
 window = Tk()
 window.title("Interface Pilote")
 
 #set windows dimensions adapted to the screen
-width = window.winfo_screenwidth()
-height = window.winfo_screenheight()
-window.geometry(f"{width}x{height}")
+width_screen = window.winfo_screenwidth()
+height_screen = window.winfo_screenheight()
+window.geometry(f"{width_screen}x{height_screen}")
+
+
+
 
 # Note : le premier paramètre passé est l'objet instancié concerné
-label = Label(window, text="Test de Label")
+label = Label(window, text="Voici notre interface SiERA")
 label.pack()
 
-quit_btn = Button(window, text="Quitter", command=window.quit)
-quit_btn.pack()
 
-frame1 = Frame(window, width=width, height=height, borderwidth=1, bg="blue")
+
+
+frame1 = Frame(window, borderwidth=2, highlightcolor="red", bg="blue")
 frame1.pack()
 
-message = Label(frame1, text="Siera window")
-message.pack(side="top", fill=X)
+quit_btn = Button(frame1, text="Quitter", command=window.quit)
+quit_btn.grid(row=0, column=0)
+
+message = Label(frame1, text="Dans le frame1")
+message.grid(row=1, column=1)
 
 
-# On démarre la boucle Tkinter qui s'interompt quand on ferme la fenêtre
+# Boucle Tkinter qui s'interompt quand on ferme la fenêtre
 mainloop()
