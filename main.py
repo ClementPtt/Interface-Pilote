75bee4a473d9257d2a69544b7577a4d00bfd51aa
lolorahaingo <lolorahaingo@hotmail.fr>
2020-11-01 20:17:37 +0100

    ajustement du positionnement des sections

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # @Date:   2020-10-15T19:31:27+02:00
-# @Last modified time: 2020-11-01T19:13:14+01:00
+# @Last modified time: 2020-11-01T20:17:04+01:00
 
 from tkinter import *
 from PIL import *
@@ -16,6 +16,9 @@ height_screen = window.winfo_screenheight()
 window.geometry(f"{width_screen}x{height_screen}")
 
 
+
+
+
 # DONNEES MOTEUR
 # - Signal fuel ON (contact voiture)
 # - Température moteur
@@ -24,7 +27,7 @@ window.geometry(f"{width_screen}x{height_screen}")
 # - Rotation par minute
 
 motor_frame = LabelFrame(window, text="DONNÉES MOTEUR", padx=5, pady=5)
-motor_frame.place(x=0, y=0)
+motor_frame.pack(side=LEFT, anchor=NW)
 
 fuel_on = Label(motor_frame, text="Signal fuel ON")
 fuel_on.grid(column=0, row=0)
@@ -38,13 +41,27 @@ rpm = Label(motor_frame, text="Rotation par minute")
 rpm.grid(column=0, row=1)
 
 
+# TEMPS
+# - temps par tour
+# - chronometre depuis le debut du tour
+
+time_frame = LabelFrame(window, text="TEMPS", padx=5, pady=5)
+time_frame.pack(side=TOP, anchor=E)
+
+time_round = Label(time_frame, text="Temps par tour")
+time_round.grid(column=0, row=0)
+chrono_round = Label(time_frame, text="Chrono depuis le début du tour")
+chrono_round.grid(column=1, row=0)
+
+
 # VITESSES
 # - Vitesse instantanée
 # - Vitesse moyenne tour actuel
 # - Vitesse moyenne tour précédent
 
 speed_frame = LabelFrame(window, text="VITESSES", padx=5, pady=5)
-speed_frame.place(relx=1/3, rely=1/3)
+speed_frame.place(relx=.5, rely=.5, anchor=CENTER)
+# speed_frame.pack()
 
 speed_round = Label(speed_frame, text="Vitesse moyenne tour actuel")
 speed_round.grid(column=0, row=0)
@@ -54,25 +71,12 @@ speed_last_round = Label(speed_frame, text="Vitesse moyenne tour précédent")
 speed_last_round.grid(column=0, row=2)
 
 
-# TEMPS
-# - temps par tour
-# - chronometre depuis le debut du tour
-
-time_frame = LabelFrame(window, text="TEMPS", padx=5, pady=5)
-time_frame.place(relx=2/3, rely=2/3)
-
-time_round = Label(time_frame, text="Temps par tour")
-time_round.grid(column=0, row=0)
-chrono_round = Label(time_frame, text="Chrono depuis le début du tour")
-chrono_round.grid(column=1, row=0)
-
-
 # AUTRES
 # - Tension de batterie
 # - Données de sonde lambda
 
 others_frame = LabelFrame(window, text="AUTRES", padx=5, pady=5)
-others_frame.place(relx=2/3, rely=0)
+others_frame.pack(side=RIGHT, anchor=S)
 
 battery_tension = Label(others_frame, text="Tension de la batterie")
 battery_tension.grid(column=0, row=0)
@@ -83,9 +87,6 @@ lambda_sens.grid(column=1, row=0)
 
 
 
-
-exit_btn = Button(window, text="Quitter", command=window.quit, fg="red")
-exit_btn.place(relx=1/3, rely=2/3)
 
 # Boucle Tkinter qui s'interompt quand on ferme la fenêtre
 window.mainloop()
