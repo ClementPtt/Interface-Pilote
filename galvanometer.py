60930ca0976924fac817af9c6448e158b4216f20
lolorahaingo <lolorahaingo@hotmail.fr>
2020-11-03 18:28:19 +0100

    documentation du README sur galvanometer.py

---
+++ galvanometer.py
@@ -0,0 +1,8 @@
+# @Author: laurent
+# @Date:   2020-11-03T18:25:22+01:00
+# @Last modified by:   laurent
+# @Last modified time: 2020-11-03T18:27:56+01:00
+
+from tkinter import *
+from PIL import *
+import os
