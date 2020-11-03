3760c708320432e89a1cbcec86d24646596c0819
lolorahaingo <lolorahaingo@hotmail.fr>
2020-11-03 19:04:34 +0100

    nouvel essai

--- galvanometer.py
+++ galvanometer.py
@@ -1,7 +1,7 @@
 # @Author: laurent
 # @Date:   2020-11-03T18:25:22+01:00
 # @Last modified by:   laurent
-# @Last modified time: 2020-11-03T18:56:16+01:00
+# @Last modified time: 2020-11-03T19:01:39+01:00
 
 from tkinter import *
 from PIL import *
@@ -11,9 +11,10 @@ import time
 import random
 
 class Galva(Canvas):
-    def __init__(self):
+    def __init__(self, frame):
         global valeur, aiguille, txt
 
-        Canvas.__init__(self)
+        canv = Canvas(frame, bg="white", height=200, width=200)
         #Dessin du Galva
-        self.create_rectangle(0,0,200,200,fill="white",outline="red")
+        canv.create_rectangle(0,0,200,200,fill="white",outline="red")
+        canv.pack()
