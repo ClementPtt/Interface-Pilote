b3ca3f283dafce5ac912f71a6abbaf3680a36112
lolorahaingo <lolorahaingo@hotmail.fr>
2020-11-04 20:44:59 +0100

    chrono added (beta)

---
+++ includes/chrono.py
@@ -0,0 +1,10 @@
+# @Date:   2020-11-04T19:58:28+01:00
+# @Last modified time: 2020-11-04T20:00:44+01:00
+
+from tkinter import *
+import time
+
+
+class Dashboard:
+    def __init__(self):
+        pass
