771f48c5423a721745071f107b5cffaea37fd04d
ClementPtt <clement.poutret@gmail.com>
2020-11-04 21:04:15 +0100

    Placement boutton

--- includes/chrono.py
+++ includes/chrono.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import time
 
+
 class Chrono(Frame):
     def __init__(self, master):
         self.frame = LabelFrame(master, text="Chrono", padx=10, pady=10)
@@ -9,11 +10,9 @@ class Chrono(Frame):
         self.titre = Label(self.frame, text="Chronometre")
         self.titre.grid(column=0, row=0)
 
-        self.play_pause_button = Button(master, text="Play", command=self.play_pause)
+        self.play_pause_button = Button(self.frame, text="Play", command=self.play_pause)
         self.play_pause_bin = 0
-        self.play_pause_button.pack()
-
-
+        self.play_pause_button.grid(column=0, row=1)
 
     def play_pause(self):
         if self.play_pause_bin == 0:
@@ -22,5 +21,3 @@ class Chrono(Frame):
         else:
             self.play_pause_button.config(text="Play")
             self.play_pause_bin = 0
-
-
