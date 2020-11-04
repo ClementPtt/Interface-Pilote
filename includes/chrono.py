7505796f7a1e05228ed54c8435ddd6dbb8b429fd
ClementPtt <clement.poutret@gmail.com>
2020-11-04 21:45:11 +0100

    chronometre (beta)

--- includes/chrono.py
+++ includes/chrono.py
@@ -10,14 +10,33 @@ class Chrono(Frame):
         self.titre = Label(self.frame, text="Chronometre")
         self.titre.grid(column=0, row=0)
 
+        self.chronometre = Label(self.frame)
+        self.chronometre.grid(column=0, row=1)
+
         self.play_pause_button = Button(self.frame, text="Play", command=self.play_pause)
         self.play_pause_bin = 0
-        self.play_pause_button.grid(column=0, row=1)
+        self.play_pause_button.grid(column=0, row=2)
+
+        self.start = time.time() + 1
+
+
+
+
 
     def play_pause(self):
         if self.play_pause_bin == 0:
             self.play_pause_button.config(text="Pause")
             self.play_pause_bin = 1
+            self.cunt()
         else:
             self.play_pause_button.config(text="Play")
             self.play_pause_bin = 0
+
+
+            self.cunt()
+
+    def cunt(self):
+        self.compteur = time.time() - self.start
+        self.chronometre.config(text=self.compteur)
+        self.chronometre.after(1000, self.cunt)
+
