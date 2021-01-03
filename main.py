42684999d6dfaaff8c1751d969d3ff43e4bb20fc
Theo45600 <73933574+Theo45600@users.noreply.github.com>
2021-01-03 13:42:37 +0100

    Ajout du menu Test dans le main

--- main.py
+++ main.py
@@ -2,11 +2,12 @@
 # @Last modified time: 2020-12-26T18:44:01+01:00
 
 from includes.dashboard import *
+from includes.test import *
 from tkinter import *
 
 def main():
     window = Tk()
-    dash = Dashboard(window)
+    dash = Dashboard(window) #Pour l'instant, choisir Dashboard ou Test pour lancer un des deux menus
     window.mainloop()
 
 if __name__ == '__main__':
