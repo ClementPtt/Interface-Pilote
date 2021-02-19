3a7d08ddecc03604770a6e179f7d6be82bf32ba8
Theo45600 <73933574+Theo45600@users.noreply.github.com>
2021-02-19 14:05:57 +0100

    affichage plein écran pour éviter les beugs de résolution

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from tkinter import *
 def main():
     window = Tk()
     dash = Dashboard(window) #Pour l'instant, choisir Dashboard ou Test pour lancer un des deux menus
+    window.attributes('-fullscreen',True) #affichage plein écran, pour éviter les beugs de résolution d'éce
     window.mainloop()
 
 if __name__ == '__main__':
