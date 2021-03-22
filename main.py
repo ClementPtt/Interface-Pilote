54106cb61a182bf99add9d90f2bc17442f6a64a1
lolorahaingo <lolorahaingo@hotmail.fr>
2021-03-22 12:07:44 +0100

    ajout de la touche <ESC> pour fermer le prgme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # @Date:   2020-11-04T16:21:54+01:00
-# @Last modified time: 2021-03-02T19:45:53+01:00
+# @Last modified time: 2021-03-22T12:07:20+01:00
 
 from includes.dashboard import *
 from includes.test import *
@@ -8,7 +8,8 @@ from tkinter import *
 def main():
     window = Tk()
     dash = Test(window) #Pour l'instant, choisir Dashboard ou Test pour lancer un des deux menus
-    window.attributes('-fullscreen',True) #affichage plein écran, pour éviter les beugs de résolution d'éce
+    window.attributes('-fullscreen',True) #affichage plein écran
+    window.bind("<Escape>", lambda e: window.destroy()) #<ESC> pour pouvoir fermer le programe
     window.mainloop()
 
 if __name__ == '__main__':
